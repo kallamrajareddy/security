d1be1778be74e071ba9658c0cc954e617b910d48
Kallam Rajendra Reddy <kallamrajareddy@gmail.com>
2020-03-26 20:21:13 +0530

    Working on tomcat

---
+++ kallam-security-app/src/main/java/com/kallam/api/security/KallamSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.kallam.api.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KallamSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KallamSecurityAppApplication.class, args);
+	}
+
+}
---
+++ kallam-security-app/src/test/java/com/kallam/api/security/KallamSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kallam.api.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KallamSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
